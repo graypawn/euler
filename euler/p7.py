2969364227aa7426a8a2cb56898165156f6875d2
graypawn <choi.pawn@gmail.com>
2015-10-12 20:42:30 +0900

    Fix p7 for improvement isPrime

--- euler/p7.py
+++ euler/p7.py
@@ -1,18 +1,10 @@
 import math
 import itertools as it
 
-def nth(iterable, n, default=None):
-    "Returns the nth item or a default value"
-    return next(it.islice(iterable, n, None), default)
-
-def prime():
-    yield 2
-    def isPrime(n):
-        return all(map(lambda x:n%x!=0,range(2,int(math.sqrt(n))+1)))
-    for i in filter(isPrime,it.count(3,2)):
-        yield i
+def isPrime(n):
+    return all(map(lambda x:n%x!=0,list(range(3,int(math.sqrt(n))+1,2))))
 
 def question(n):
-    return next(it.islice(prime(),n-1,None))
+    return next(it.islice(filter(isPrime,it.count(3,2)),n-2,None))
 
 print(question(10001))
