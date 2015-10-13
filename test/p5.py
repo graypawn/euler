282a6e866256cda88af423059e82785b4543e317
graypawn <choi.pawn@gmail.com>
2015-10-13 22:32:35 +0900

    delete test

--- test/p5.py
+++
@@ -1,4 +0,0 @@
-import euler.p5 as p
-
-def test_p5():
-    assert p.question(10) == 2520
--- test/p7.py
+++
@@ -1,5 +0,0 @@
-import euler.p7 as p
-
-def test_p7():
-    assert p.question(5) == 11
-    assert p.question(6) == 13
